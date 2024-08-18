31813d917a90852a8cd7eb5d1fad6d91d383c52f
rdanbaro <138162038+rdanbaro@users.noreply.github.com>
2024-08-18 14:02:53 -0400

    Df primer filtrado listo

--- scripts.py
+++ scripts.py
@@ -41,3 +41,31 @@ def filtrar_top_jugadores(grupo):
         return grupo.head(1)
     else:
         return pd.DataFrame()  # En caso de posiciones no especificadas
+
+def asignar_puntos_posicion(Pos):
+    
+    match Pos:
+        case 1:
+            return 10
+        case 2:
+            return 9
+        case 3:
+            return 8
+        case 4:
+            return 7
+        case 5:
+            return 6
+        case 6: 
+            return 5
+        case 7:
+            return 4
+        case 8:
+            return 3
+        case 9:
+            return 2
+        case 10:
+            return 1
+        case _:
+            return 0
+    
+    
